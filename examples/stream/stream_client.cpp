cf1bc01571a9fe16e249966dc73cf3e1b0b095a5
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2024-01-25 02:12:46 +0300

    Update stream_client.cpp

--- examples/stream/stream_client.cpp
+++ examples/stream/stream_client.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
+#include <fstream>
+#include <streambuf>
 
 // так тоже можн поднять сервер
 //g++ -o stream_server -std=c++11 stream_server.cpp -I./websocketpp -lboost_system -lpthread
@@ -30,10 +32,33 @@ int main() {
         std::cout << "Received Message: " << msg->get_payload() << std::endl;
     }, _1, _2));
 
+    std::ifstream myfile("myfile2.txt");
+    if (myfile.is_open()) {
+        // Читаем и выводим содержимое файла в консоль
+        std::cout << "File content:\n";
+        std::cout << std::string((std::istreambuf_iterator<char>(myfile)), std::istreambuf_iterator<char>()) << std::endl;
+
+        // Закрываем файл
+        myfile.close();
+    } else {
+        std::cerr << "Error opening myfile2.txt" << std::endl;
+    }
+
+
     // Отправить сообщение на сервер после успешного подключения
     echo_client.set_open_handler([&](websocketpp::connection_hdl hdl) {
         std::string message = "Hello, server 444444444!";
-        echo_client.send(hdl, message, websocketpp::frame::opcode::text);
+        std::ifstream myfile("myfile2.txt");
+        std::string message_2((std::istreambuf_iterator<char>(myfile)), std::istreambuf_iterator<char>());
+        std::cout <<"MESSAGE 2 : " << message_2 << std::endl;
+        // Закрываем файл
+        myfile.close();
+
+        echo_client.send(hdl, message_2, websocketpp::frame::opcode::text);
+        //echo_client.send(hdl, message_2, websocketpp::frame::opcode::text, websocketpp::frame::flag::utf8);
+        //echo_client.send(hdl, message_2, websocketpp::frame::opcode::text, websocketpp::frame::flag::TEXT);
+
+
     });
 
     // Get a connection to the desired server
