41f9f20675d13158237b2c6b3091fde3bb5a29c3
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2024-01-25 23:59:24 +0300

    Update stream_client.cpp

--- examples/stream/stream_client.cpp
+++ examples/stream/stream_client.cpp
@@ -4,78 +4,101 @@
 #include <fstream>
 #include <streambuf>
 #include <filesystem>
+#include <thread>
 
-// так тоже можн поднять сервер
-//g++ -o stream_server -std=c++11 stream_server.cpp -I./websocketpp -lboost_system -lpthread
-//g++ -o stream_client -std=c++11 stream_client.cpp -I./websocketpp -lboost_system -lpthread
-//./stream_client
-//./stream_server
+namespace ws = websocketpp;
+
+// Create a client endpoint
+typedef ws::client<ws::config::asio_client> client;
+
+// Set up a client
+client echo_client;
+
+// Variable to store the previous content of the file
+static std::string previous_file_content;
+
+// Mutex for synchronizing access to common resources
+std::mutex file_mutex;
+
+// Define a function to send changes to the server
+void send_changes_to_server(ws::connection_hdl hdl) {
+    std::cout << "2 LOOP: " << std::endl;
+    std::ifstream myfile("myfile2.txt");
+    if (myfile.is_open()) {
+        std::cout << "3 LOOP: " << std::endl;
+        // Read the content of the file
+        std::string current_file_content((std::istreambuf_iterator<char>(myfile)), std::istreambuf_iterator<char>());
+        myfile.close();
+        std::cout << "4 LOOP: " << std::endl;
+
+        // Find differences relative to the previous content
+        std::size_t pos = previous_file_content.size();
+        std::string changes = current_file_content.substr(pos);
+        std::cout << "5 LOOP: " << std::endl;
+
+        // Use a mutex to synchronize access to common resources
+        std::lock_guard<std::mutex> lock(file_mutex);
+
+        // Send changes to the server
+        if (!changes.empty()) {
+            std::cout << "6 LOOP: " << std::endl;
+            echo_client.send(hdl, changes, ws::frame::opcode::text);
+            std::cout << "Sent changes to server." << std::endl;
+        } else {
+            std::cout << "No changes to send." << std::endl;
+        }
+
+        // Update the previous content
+        std::cout << "7 LOOP: " << std::endl;
+        previous_file_content = current_file_content;
+    } else {
+        std::cerr << "Error opening myfile2.txt" << std::endl;
+    }
+}
 
 int main() {
-    using websocketpp::lib::placeholders::_1;
-    using websocketpp::lib::placeholders::_2;
-    using websocketpp::lib::bind;
-
-    // Create a client endpoint
-    typedef websocketpp::client<websocketpp::config::asio_client> client;
-
-    // Set up a client
-    client echo_client;
+    using ws::lib::placeholders::_1;
+    using ws::lib::placeholders::_2;
+    using ws::lib::bind;
 
     // Initialize the client
     echo_client.init_asio();
     std::cout << "START CLIENT" << std::endl;
 
     // Register our message handler
-    echo_client.set_message_handler(bind([&](websocketpp::connection_hdl hdl, client::message_ptr msg) {
+    echo_client.set_message_handler(bind([&](ws::connection_hdl hdl, client::message_ptr msg) {
         std::cout << "Received Message: " << msg->get_payload() << std::endl;
     }, _1, _2));
 
-    std::ifstream myfile("myfile2.txt");
-
-
-    // Variable to store the previous content of the file
-    static std::string previous_file_content;
-
-    // Define a function to send changes to the server
-    auto send_changes_to_server = [&](websocketpp::connection_hdl hdl) {
-        std::cout << "2 LOOP: " << std::endl;
-        std::ifstream myfile("myfile2.txt");
-        if (myfile.is_open()) {
-            std::cout << "3 LOOP: " << std::endl;
-            // Read the content of the file
-            std::string current_file_content((std::istreambuf_iterator<char>(myfile)), std::istreambuf_iterator<char>());
-            myfile.close();
-
-            // Find differences relative to the previous content
-            std::size_t pos = previous_file_content.size();
-            std::string changes = current_file_content.substr(pos);
-
-            // Send changes to the server
-            if (!changes.empty()) {
-                echo_client.send(hdl, changes, websocketpp::frame::opcode::text);
-                std::cout << "Sent changes to server." << std::endl;
-            } else {
-                std::cout << "No changes to send." << std::endl;
-            }
-
-            // Update the previous content
-            previous_file_content = current_file_content;
-        } else {
-            std::cerr << "Error opening myfile2.txt" << std::endl;
-        }
-    };
-
     // Send a message to the server after successful connection
-    echo_client.set_open_handler([&](websocketpp::connection_hdl hdl) {
+    echo_client.set_open_handler([&](ws::connection_hdl hdl) {
         std::cout << "Connected to the server" << std::endl;
 
-        // Send changes to the server
-        send_changes_to_server(hdl);
+        // Start a thread to check for changes in the file and send them to the server
+        std::thread file_check_thread([&]() {
+            while (true) {
+                // Wait for 3 seconds
+                std::this_thread::sleep_for(std::chrono::seconds(3));
+                std::cout << "1 LOOP: " << std::endl;
+
+                // Check the file and send changes to the server
+                echo_client.get_io_service().post([hdl]() {
+                    send_changes_to_server(hdl);
+                });
+            }
+        });
+
+        // Join the thread after the client.run() completes
+        echo_client.set_close_handler([&](ws::connection_hdl hdl) {
+            file_check_thread.join();
+        });
+
+        // Start the thread to check the file
+        file_check_thread.detach();
     });
 
     // Get a connection to the desired server
-    websocketpp::lib::error_code ec;
+    ws::lib::error_code ec;
     auto con = echo_client.get_connection("ws://localhost:9002", ec);
 
     if (ec) {
@@ -85,18 +108,6 @@ int main() {
 
     // Run the client
     echo_client.connect(con);
-    std::cout << "SET CONNECTION to server and go to LOOP: " << std::endl;
-    // Start a loop to check for changes in the file and send them to the server
-    while (true) {
-        // Wait for 15 seconds
-        std::this_thread::sleep_for(std::chrono::seconds(3));
-        std::cout << "1 LOOP: " << std::endl;
-        // Check the file and send changes to the server
-        echo_client.get_io_service().post([con, &send_changes_to_server]() {
-            send_changes_to_server(con);
-        });
-    }
-
     echo_client.run();
 
     return 0;
