9aa9f129517825a23f7a39013590b0ad9cc51e84
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2024-01-26 22:05:28 +0300

    Update stream_client.cpp

--- examples/stream/stream_client.cpp
+++ examples/stream/stream_client.cpp
@@ -3,6 +3,7 @@
 #include <websocketpp/client.hpp>
 #include <fstream>
 #include <thread>
+#include <atomic>
 
 typedef websocketpp::client<websocketpp::config::asio_client> client;
 
@@ -21,33 +22,28 @@ bool file_content_changed(const std::string& current_data, const std::string& pr
 }
 
 std::string previous_data;
+std::atomic<bool> ready(false);
 
 void send_data_thread(client& echo_client, websocketpp::connection_hdl hdl) {
-    std::cout << "START Sending data: " << std::endl;
-    try {
-        while (is_connection_open(echo_client, hdl)) {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            std::cout << "START read msg: " << std::endl;
-            std::ifstream myfile("myfile2.txt");
-            std::string message((std::istreambuf_iterator<char>(myfile)), std::istreambuf_iterator<char>());
-            myfile.close();
+    while (!ready) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
 
-            std::cout << "msg: "  << message<< std::endl;
+    while (is_connection_open(echo_client, hdl)) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
-            if (file_content_changed(message, previous_data)) {
-                std::cout << "Sending data: " << message << std::endl;
-                echo_client.send(hdl, message, websocketpp::frame::opcode::text);
-                previous_data = message;
-            }
-            std::cout << "DID SEND DATA" << std::endl;
-            //std::cout << "Sending data: " << message << std::endl;
+        std::ifstream myfile("myfile2.txt");
+        std::string message((std::istreambuf_iterator<char>(myfile)), std::istreambuf_iterator<char>());
+        myfile.close();
 
-            //echo_client.send(hdl, message, websocketpp::frame::opcode::text);
+        std::cout << "msg: " << message << std::endl;
 
-            std::this_thread::sleep_for(std::chrono::seconds(1));
+        if (file_content_changed(message, previous_data)) {
+            std::cout << "Sending data: " << message << std::endl;
+            echo_client.send(hdl, message, websocketpp::frame::opcode::text);
+            previous_data = message;
         }
-    } catch (const websocketpp::lib::error_code& e) {
-        std::cout << "NOT connection_open: " << e << "(" << e.message() << ")" << std::endl;
+        std::cout << "DID SEND DATA" << std::endl;
     }
 }
 
@@ -80,19 +76,22 @@ int main() {
     std::cout << "try send_thread: " << std::endl;
     std::thread send_thread(send_data_thread, std::ref(echo_client), con->get_handle());
     std::cout << "did send_thread: " << std::endl;
+
     // Connect to the server
     std::cout << "try connect " << std::endl;
     echo_client.connect(con);
     std::cout << "did connect " << std::endl;
 
+    ready = true;  // Set the flag to indicate that the sending thread can start
+
     // Run the client
     std::cout << "try run " << std::endl;
     echo_client.run();
     std::cout << "did run " << std::endl;
+
     // Wait for the send thread to finish
     send_thread.join();
     std::cout << "ALL THREAD RUN: " << std::endl;
-
 
     return 0;
 }
