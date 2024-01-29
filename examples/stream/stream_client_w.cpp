954488614c03da2c315682cf5d9f92a6397e6d1c
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2024-01-29 23:53:51 +0300

    Update stream_client_w.cpp

--- examples/stream/stream_client_w.cpp
+++ examples/stream/stream_client_w.cpp
@@ -31,6 +31,37 @@ auto client_thread_function = [](client* echo_client) {
     echo_client->stop();  // Используйте метод stop вместо close
 };
 
+// Определение функции для запуска клиента в отдельном потоке
+/*
+auto client_thread_function = [&echo_client]() {
+    // Инициализация клиента
+    echo_client.init_asio();
+    std::cout << "START CLIENT" << std::endl;
+
+    // Установка обработчиков
+    echo_client.set_message_handler([](websocketpp::connection_hdl hdl, websocketpp::client<websocketpp::config::asio_client>::message_ptr msg) {
+        std::cout << "Received Message: " << msg->get_payload() << std::endl;
+    });
+
+    // Получение соединения с сервером
+    websocketpp::lib::error_code ec;
+    auto con = echo_client.get_connection("ws://localhost:9002", ec);
+
+    if (ec) {
+        std::cout << "Could not create connection because: " << ec.message() << std::endl;
+        return;
+    }
+
+    // Подключение к серверу
+    echo_client.connect(con);
+
+    // Запуск клиента
+    echo_client.run();
+};
+*/
+
+//  500 -> 00:05.000
+// 6000 -> 01:00.000
 std::string to_timestamp(int64_t t) {
     int64_t sec = t/100;
     int64_t msec = t - sec*100;
@@ -254,7 +285,7 @@ int audio_processing_function(int argc, char ** argv, client * echo_client, clie
 
         wavWriter.open(filename, WHISPER_SAMPLE_RATE, 16, 1);
     }
-
+    printf("[Start speaking]\n");
     fflush(stdout);
 
     auto t_last  = std::chrono::high_resolution_clock::now();
@@ -262,18 +293,217 @@ int audio_processing_function(int argc, char ** argv, client * echo_client, clie
 
     // main audio loop
     while (is_running) {
-        printf("[Start speaking]\n");
         websocketpp::connection_hdl hdl;
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+        /*
+        if (params.save_audio) {
+            wavWriter.write(pcmf32_new.data(), pcmf32_new.size());
+            // Отправка данных на сервер
+            echo_client->send((*con)->get_handle(), pcmf32_new.data(), pcmf32_new.size() * sizeof(float), websocketpp::frame::opcode::binary);
+        }
+        */
+        // handle Ctrl + C
+        is_running = sdl_poll_events();
+
+        if (!is_running) {
+            break;
+        }
+
+        // process new audio
+
+        if (!use_vad) {
+            while (true) {
+                audio.get(params.step_ms, pcmf32_new);
+                echo_client->send((*con)->get_handle(), pcmf32_new.data(), pcmf32_new.size() * sizeof(float), websocketpp::frame::opcode::binary);
 
 
-        wavWriter.write(pcmf32_new.data(), pcmf32_new.size());
-        // Отправка данных на сервер
-        std::cout << "try write bytes" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-        echo_client->send((*con)->get_handle(), pcmf32_new.data(), pcmf32_new.size() * sizeof(float), websocketpp::frame::opcode::binary);
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-        std::cout <<"Bytes sended to server" << std::endl;
+                if ((int) pcmf32_new.size() > 2*n_samples_step) {
+                    fprintf(stderr, "\n\n%s: WARNING: cannot process audio fast enough, dropping audio ...\n\n", __func__);
+                    audio.clear();
+                    continue;
+                }
+
+                if ((int) pcmf32_new.size() >= n_samples_step) {
+                    audio.clear();
+                    break;
+                }
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+
+            const int n_samples_new = pcmf32_new.size();
+
+            // take up to params.length_ms audio from previous iteration
+            const int n_samples_take = std::min((int) pcmf32_old.size(), std::max(0, n_samples_keep + n_samples_len - n_samples_new));
+
+            //printf("processing: take = %d, new = %d, old = %d\n", n_samples_take, n_samples_new, (int) pcmf32_old.size());
+
+            pcmf32.resize(n_samples_new + n_samples_take);
+
+            for (int i = 0; i < n_samples_take; i++) {
+                pcmf32[i] = pcmf32_old[pcmf32_old.size() - n_samples_take + i];
+            }
+
+            memcpy(pcmf32.data() + n_samples_take, pcmf32_new.data(), n_samples_new*sizeof(float));
+
+            pcmf32_old = pcmf32;
+        } else {
+            const auto t_now  = std::chrono::high_resolution_clock::now();
+            const auto t_diff = std::chrono::duration_cast<std::chrono::milliseconds>(t_now - t_last).count();
+
+            if (t_diff < 2000) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+                continue;
+            }
+
+            audio.get(2000, pcmf32_new);
+            echo_client->send((*con)->get_handle(), pcmf32_new.data(), pcmf32_new.size() * sizeof(float), websocketpp::frame::opcode::binary);
+
+
+            if (::vad_simple(pcmf32_new, WHISPER_SAMPLE_RATE, 1000, params.vad_thold, params.freq_thold, false)) {
+                audio.get(params.length_ms, pcmf32);
+            } else {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+                continue;
+            }
+
+            t_last = t_now;
+        }
+
+        // run the inference
+
+//        {
+//            whisper_full_params wparams = whisper_full_default_params(WHISPER_SAMPLING_GREEDY);
+//
+//            wparams.print_progress   = false;
+//            wparams.print_special    = params.print_special;
+//            wparams.print_realtime   = false;
+//            wparams.print_timestamps = !params.no_timestamps;
+//            wparams.translate        = params.translate;
+//            wparams.single_segment   = !use_vad;
+//            wparams.max_tokens       = params.max_tokens;
+//            wparams.language         = params.language.c_str();
+//            wparams.n_threads        = params.n_threads;
+//
+//            wparams.audio_ctx        = params.audio_ctx;
+//            wparams.speed_up         = params.speed_up;
+//
+//            wparams.tdrz_enable      = params.tinydiarize; // [TDRZ]
+//
+//            // disable temperature fallback
+//            //wparams.temperature_inc  = -1.0f;
+//            wparams.temperature_inc  = params.no_fallback ? 0.0f : wparams.temperature_inc;
+//
+//            wparams.prompt_tokens    = params.no_context ? nullptr : prompt_tokens.data();
+//            wparams.prompt_n_tokens  = params.no_context ? 0       : prompt_tokens.size();
+//
+//            if (whisper_full(ctx, wparams, pcmf32.data(), pcmf32.size()) != 0) {
+//                fprintf(stderr, "%s: failed to process audio\n", argv[0]);
+//                return 6;
+//            }
+//
+//            // print result;
+//            {
+//                if (!use_vad) {
+//                    printf("\33[2K\r");
+//
+//                    // print long empty line to clear the previous line
+//                    printf("%s", std::string(100, ' ').c_str());
+//
+//                    printf("\33[2K\r");
+//                } else {
+//                    const int64_t t1 = (t_last - t_start).count()/1000000;
+//                    const int64_t t0 = std::max(0.0, t1 - pcmf32.size()*1000.0/WHISPER_SAMPLE_RATE);
+//
+//                    printf("\n");
+//                    printf("### Transcription %d START | t0 = %d ms | t1 = %d ms\n", n_iter, (int) t0, (int) t1);
+//                    printf("\n");
+//                }
+//
+//                const int n_segments = whisper_full_n_segments(ctx);
+//                for (int i = 0; i < n_segments; ++i) {
+//                    const char * text = whisper_full_get_segment_text(ctx, i);
+//
+//                    // Открытие файла для записи
+//                    //
+//
+//                    // Запись распознанного текста в файл
+//                    std::cout << "Распознанный текст: " << text <<std::endl;
+//
+//
+//                    outFile << text << std::endl;
+//
+//                    /* Отправляем данные серверу */
+//                    //echo_client->send((*con)->get_handle(), text, websocketpp::frame::opcode::text);
+//
+//                    // Закрытие файла
+//                    //
+//
+//
+//
+//                    if (params.no_timestamps) {
+//                        printf("%s", text);
+//                        fflush(stdout);
+//
+//                        if (params.fname_out.length() > 0) {
+//                            fout << text;
+//                        }
+//                    } else {
+//                        const int64_t t0 = whisper_full_get_segment_t0(ctx, i);
+//                        const int64_t t1 = whisper_full_get_segment_t1(ctx, i);
+//
+//                        std::string output = "[" + to_timestamp(t0) + " --> " + to_timestamp(t1) + "]  " + text;
+//
+//                        if (whisper_full_get_segment_speaker_turn_next(ctx, i)) {
+//                            output += " [SPEAKER_TURN]";
+//                        }
+//
+//                        output += "\n";
+//
+//                        printf("%s", output.c_str());
+//                        fflush(stdout);
+//
+//                        if (params.fname_out.length() > 0) {
+//                            fout << output;
+//                        }
+//                    }
+//                }
+//
+//                if (params.fname_out.length() > 0) {
+//                    fout << std::endl;
+//                }
+//
+//                if (use_vad) {
+//                    printf("\n");
+//                    printf("### Transcription %d END\n", n_iter);
+//                }
+//            }
+//
+//            ++n_iter;
+//
+//            if (!use_vad && (n_iter % n_new_line) == 0) {
+//                printf("\n");
+//
+//                // keep part of the audio for next iteration to try to mitigate word boundary issues
+//                pcmf32_old = std::vector<float>(pcmf32.end() - n_samples_keep, pcmf32.end());
+//
+//                // Add tokens of the last full length segment as the prompt
+//                if (!params.no_context) {
+//                    prompt_tokens.clear();
+//
+//                    const int n_segments = whisper_full_n_segments(ctx);
+//                    for (int i = 0; i < n_segments; ++i) {
+//                        const int token_count = whisper_full_n_tokens(ctx, i);
+//                        for (int j = 0; j < token_count; ++j) {
+//                            prompt_tokens.push_back(whisper_full_get_token_id(ctx, i, j));
+//                        }
+//                    }
+//                }
+//            }
+//            fflush(stdout);
+//        }
+
 
     }
 
